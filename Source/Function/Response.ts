ee266d0cdd37cdd185f4925963a92eb62859eabf
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-19 14:16:15 +0200

    squash!

--- Source/Function/Response.ts
+++ Source/Function/Response.ts
@@ -2,13 +2,13 @@
  * @module Response
  *
  */
-export default (async (...[Message = null, Status = 200]: Parameters<Type>) =>
+export default ((async (...[Message = null, Status = 200]: Parameters<Type>) =>
 	new Response(JSON.stringify(Message), {
 		status: Status,
 		headers: {
 			"Content-Type": "application/json;charset=utf-8",
 		},
-	})) satisfies Type as Type;
+	})) satisfies Type as Type);
 
 import type Type from "../Interface/Response.js";
 
